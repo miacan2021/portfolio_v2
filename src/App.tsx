87ba47d7b096da28fef25cb50cdb763fa437ae8a
Shiho(mia) <mia.can2021@gmail.com>
2025-01-28 10:55:15 -0800

    add favicon and logo

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import "./App.css";
 
 function App() {
   return (
-    <>
+    <div className="bg-primary w-screen min-h-screen flex justify-center items-center overflow-x-hidden">
       <h1 className=" text-orange font-title text-3xl ">portfolio</h1>
       <h1 className=" text-blue font-text text-3xl ">portfolio</h1>
-    </>
+    </div>
   );
 }
 
