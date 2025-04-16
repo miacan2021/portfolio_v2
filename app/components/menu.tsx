f879d3584e955c6010d3bf7d5349e96a59359023
Shiho(mia) <mia.can2021@gmail.com>
2025-04-15 21:09:25 -0700

    Add: markup for menu component

---
+++ app/components/menu.tsx
@@ -0,0 +1,16 @@
+import React, { ReactElement } from "react";
+
+const menu = (): ReactElement => {
+  return (
+    <header>
+      <a>home</a>
+      <a>works</a>
+      <a>contact</a>
+      <a>github</a>
+      <a>linkedin</a>
+      <a>resume</a>
+    </header>
+  );
+};
+
+export default menu;
