7764ead3d9515677c69d474e8f817d9236f3f1b6
Shiho(mia) <mia.can2021@gmail.com>
2025-05-19 22:29:51 -0700

    feat: add responsive styling to menu component
    Apply tailwind utility classes for mobile and desktop layouts
    toggle visivility with button click on mobile layouts

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
+import HeaderMenu from "./components/menu";
+
 export default function Home() {
   return (
-    <div>
+    <div className="bg-beige min-h-screen">
+      <HeaderMenu />
       <h1 className="font-anton text-orange">Hello</h1>
       <p className="font-roboto text-sky">test</p>
     </div>
